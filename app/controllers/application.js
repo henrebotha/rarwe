0770abe8957585b0312d729e30a3d1120d95b253
Henre Botha <henrebotha@gmail.com>
2015-09-22 17:06:53 +0200

    Define Song & SongCollection

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+var Song = Ember.Object.extend({
+	title:  '',
+	rating: 0,
+	band:   ''
+});
+
+var SongCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
+	sortProperties: ['rating'],
+	sortAscending:  false,
+	content:        []
+});
+
+
+
 export default Ember.Controller.extend({
 });
